70364479bf94e76030fc73d3f599daa38de8b4d7
alyuza <alyuza.sat@gmail.com>
2023-08-26 20:37:01 +0700

    modify app.js

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require('express');
 require('dotenv').config();
-// Import
-const OpenApiValidator = require("express-openapi-validator");
+const express = require('express');
+const databaseMiddleware = require('./middleware/database-middleware.js');
+const authRouter = require('./routes/auth-router.js');
+const transferRouter = require('./routes/transfer-router.js');
+const authMiddleware = require('./middleware/authentication-middleware.js');
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yaml");
 const fs = require("fs");
 const cors = require("cors")
+const OpenApiValidator = require("express-openapi-validator");
 
-const databaseMiddleware = require('./middleware/database-middleware.js');
-const authMiddleware = require('./middleware/authentication-middleware.js');
-const authRouter = require('./routes/auth-router.js');
-const transferRouter = require('./routes/transfer-router.js');
+const openApiPath = "apiDocsAi.yaml";
+const readApiFile = fs.readFileSync(openApiPath, "utf8");
+const swaggerDocs = yaml.parse(readApiFile); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); 
+app.use(OpenApiValidator.middleware({
+  apiSpec: openApiPath,
+  validateRequests: true,
+}))
 app.use(databaseMiddleware);
 
 app.get('/', (req, res) => {
@@ -25,15 +32,8 @@ app.get('/', (req, res) => {
 app.use('/auth', authRouter)
 app.use('/transfers', authMiddleware, transferRouter)
 
-// API Documentation
-const openApiPath = "apiDocsAi.yaml";
-const readApiFile = fs.readFileSync(openApiPath, "utf8");
-const swaggerDocs = yaml.parse(readApiFile); 
-
-// App Router
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); 
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log('Server is running on Localhost:3000')
 })
 
