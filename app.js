0ac6e68d48032945fb01bc3d1180feb9a684316d
alyuza <alyuza.sat@gmail.com>
2023-08-26 20:01:51 +0700

    testing

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
-require('dotenv').config();
-// Import
-const OpenApiValidator = require("express-openapi-validator");
-const swaggerUi = require("swagger-ui-express");
-const yaml = require("yaml");
-const fs = require("fs");
-const cors = require("cors")
-
-const databaseMiddleware = require('./middleware/database-middleware.js');
-const authMiddleware = require('./middleware/authentication-middleware.js');
-const authRouter = require('./routes/auth-router.js');
-const transferRouter = require('./routes/transfer-router.js');
-
 const app = express();
+const PORT = process.env.PORT || 3000
 
-app.use(cors());
-app.use(express.json());
-app.use(databaseMiddleware);
+const uri = 'mongodb+srv://revou:yuzaimoet123@billieeilish.8qyc76s.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
 
-app.get('/', (req, res) => {
-  res.send('Week 10 Assignment - Alyuza Satrio Prayogo')
+app.get("/items/:my_item", async (req, res) => {
+    let item = await client.db("Revou")
+                .collection("users")
+                .find().toArray()
+
+    return res.json(item)
 })
 
-app.use('/auth', authRouter)
-app.use('/transfers', authMiddleware, transferRouter)
-
-// API Documentation
-const openApiPath = "apiDocsAi.yaml";
-const readApiFile = fs.readFileSync(openApiPath, "utf8");
-const swaggerDocs = yaml.parse(readApiFile); 
-
-// App Router
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); 
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
-
+client.connect(err => {
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+});
