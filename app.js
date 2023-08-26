a3e8f40778de2b26f3fa7f2b4c2f605ecac1325c
alyuza <alyuza.sat@gmail.com>
2023-08-26 21:29:12 +0700

    modify app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-require('dotenv').config();
 const express = require('express');
-const databaseMiddleware = require('./middleware/database-middleware.js');
-const authRouter = require('./routes/auth-router.js');
-const transferRouter = require('./routes/transfer-router.js');
-const authMiddleware = require('./middleware/authentication-middleware.js');
+require('dotenv').config();
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yaml");
 const fs = require("fs");
@@ -12,27 +8,36 @@ const OpenApiValidator = require("express-openapi-validator");
 
 const openApiPath = "apiDocsAi.yaml";
 const readApiFile = fs.readFileSync(openApiPath, "utf8");
-const swaggerDocs = yaml.parse(readApiFile); 
-
+const swaggerDocs = yaml.parse(readApiFile);
 const app = express();
+
+const authMiddleware = require('./middleware/authentication-middleware.js');
+const databaseMiddleware = require('./middleware/database-middleware.js');
+const authRouter = require('./routes/auth-router.js');
+const transferRouter = require('./routes/transfer-router.js');
 
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); 
-app.use(OpenApiValidator.middleware({
-  apiSpec: openApiPath,
-  validateRequests: true,
-}))
 app.use(databaseMiddleware);
 
+// homepage
 app.get('/', (req, res) => {
   res.send('Week 10 Assignment - Alyuza Satrio Prayogo')
 })
 
+// Swagger Docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// API Validator
+app.use(OpenApiValidator.middleware({
+  apiSpec: openApiPath,
+  validateRequests: true,
+}))
+
 app.use('/auth', authRouter)
 app.use('/transfers', authMiddleware, transferRouter)
 
-
+// connect to localhost
 app.listen(3000, () => {
   console.log('Server is running on Localhost:3000')
 })
